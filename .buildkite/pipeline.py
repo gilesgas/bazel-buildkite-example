450030b56153e037c7725b35f0d034bf3febc1d3
Christian Nunciato <chris@nunciato.org>
2025-03-26 18:12:02 -0700

    More comments

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -1,37 +1,45 @@
-from utils import run, only_dirs, to_paths, build_test_and_annotate, to_json
+from utils import run, filter_dirs, to_paths, make_pipeline_step, to_json
 
 # By default, do nothing.
 steps = []
 
 # Get a list of directories changed in the most recent commit.
 changed_paths = run(["git", "diff-tree", "--name-only", "HEAD~1..HEAD"])
-changed_dirs = only_dirs(changed_paths)
+changed_dirs = filter_dirs(changed_paths)
 
 # Query the Bazel workspace for a list of all packages (libraries, binaries, etc.).
 all_packages = run(["bazel", "query", "'/...'"])
 
-# Using both lists, figure out which packages need to be built, so we can build
-# pipeline steps only for those.
+# Using both lists, figure out which packages need to be built, so we can assemble
+# a pipeline that builds only those.
 changed_packages = [p for p in changed_dirs if p in to_paths(all_packages)]
 
-# For each changed package, build and test all of its targets.
+# For each of those packages, build and test all of its targets.
 for pkg in changed_packages:
-    package_step = build_test_and_annotate(pkg)
+    package_step = make_pipeline_step(pkg)
 
     # Query the package for any Python libraries.
     libraries = run(["bazel", "query", f"kind(py_library, '//{pkg}/...')"])
 
-    # If the package does contain libraries, query the Bazel graph to assemble a
-    # list of each library's reverse dependencies (i.e., the packages that
-    # depend on it), adding a step to the Buildkite pipeline dynamically to
-    # build and test each one. (Skipping the ones that have already been queued).
+    # If the package contain one or more Python libraries, query the Bazel graph
+    # to assemble a list of each library's reverse dependencies (i.e., the Bazel
+    # packages that depend on it), adding a step to the Buildkite pipeline
+    # dynamically to build and test each one -- for example, to guard against
+    # accidental regressions.
     for lib in libraries:
         reverse_deps = run(["bazel", "query", f"rdeps(//..., //{pkg}/...)"])
-        rdeps_to_build = [p for p in to_paths(reverse_deps, pkg) if p not in changed_packages]
+
+        # Skip (re-)building any reverse_deps that also belong to the
+        # changed_packages set. (There's no need to build them a second time.)
+        rdeps_to_build = [
+            p for p in to_paths(reverse_deps, pkg) if p not in changed_packages
+        ]
 
         for dep in rdeps_to_build:
-            rdep_step = build_test_and_annotate(dep, pkg)
-            package_step["commands"].append(f"""echo '{to_json({"steps": [rdep_step]})}' | buildkite-agent pipeline upload""")
+            rdep_step = make_pipeline_step(dep, pkg)
+            package_step["commands"].append(
+                f"""echo '{to_json({"steps": [rdep_step]})}' | buildkite-agent pipeline upload"""
+            )
 
     steps.append(package_step)
 
--- .buildkite/utils.py
+++ .buildkite/utils.py
@@ -1,12 +1,19 @@
 import json, os, subprocess
 
-# Runs an OS command and returns a list of the lines comprising the result.
+
+# Runs an OS command, returning stdout as a list of lines.
 def run(command):
-    return subprocess.run(command, capture_output=True, text=True, check=True).stdout.strip().splitlines()
+    return (
+        subprocess.run(command, capture_output=True, text=True, check=True)
+        .stdout.strip()
+        .splitlines()
+    )
+
 
 # Converts a list of file paths into a list of directories (omitting those that aren't).
-def only_dirs(paths):
+def filter_dirs(paths):
     return list(filter(lambda p: os.path.isdir(p), paths))
+
 
 # Converts a list of Bazel targets into a unique list of top-level paths.
 def to_paths(targets, exclude=None):
@@ -15,38 +22,49 @@ def to_paths(targets, exclude=None):
     for target in targets:
         directory, _, _ = target.rpartition(":")
         groups.add(directory.lstrip("/"))
-    
+
     if exclude:
         groups.discard(exclude)
-    
+
     return list(groups)
 
-# Defines a Buildkite `command` step given a key, emoji, label, list of
+
+# Returns a Buildkite `command` step given a key, emoji, label, list of
 # commands, and optional list of plugins.
 def command_step(key, emoji, label, commands=[], plugins=[], depends_on=None):
     step = {"key": key, "label": f":{emoji}: {label}", "commands": commands}
-    
+
     if len(plugins) > 0:
         step["plugins"] = plugins
-    
+
     if depends_on is not None:
         step["depends_on"] = depends_on
-    
+
     return step
 
-# Defines a command test that builds, tests, and annotates a given Bazel package.
-def build_test_and_annotate(package, depends_on=None):
-    return command_step(package, "bazel", f"Build and test //{package}/...", [
-        f"bazel test //{package}/...",
-        f"bazel build //{package}/... --build_event_json_file=bazel-events.json",
-    ], [{
-        "bazel-annotate#v0.1.0": {
-            "bep_file": f"bazel-events.json",
-            "skip_if_no_bep": True,
-        }
-    }], depends_on)
+
+# Returns a Buildkite `command` test that builds, tests, and annotates a given Bazel package.
+def make_pipeline_step(package, depends_on=None):
+    return command_step(
+        package,
+        "bazel",
+        f"Build and test //{package}/...",
+        [
+            f"bazel test //{package}/...",
+            f"bazel build //{package}/... --build_event_json_file=bazel-events.json",
+        ],
+        [
+            {
+                "bazel-annotate#v0.1.0": {
+                    "bep_file": f"bazel-events.json",
+                    "skip_if_no_bep": True,
+                }
+            }
+        ],
+        depends_on,
+    )
+
 
 # Converts a Python dictionary into a JSON string.
 def to_json(data, indent=None):
     return json.dumps(data, indent=indent)
-
