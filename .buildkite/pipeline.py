7d8494a00d29f9cf1149ee0aadb40348531b0054
Christian Nunciato <chris@nunciato.org>
2025-03-30 11:54:48 -0700

    Words are hard

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -17,11 +17,9 @@ changed_packages = [p for p in changed_dirs if p in get_paths(all_packages)]
 # For each changed Bazel package, assemble a pipeline step programmatically to
 # build and test all of its targets.
 #
-# Additionally, if the package defines one or more Python libraries, query the
-# Bazel graph to assemble a list of each library's reverse dependencies (i.e.,
-# the Bazel packages that depend on it), adding a step to the Buildkite pipeline
-# at runtime to build and test each of those as well -- for example, to guard
-# against downstream regressions.
+# If the package contains any Python libraries, also query their own Bazel
+# graphs to build a list of their reverse dependencies (i.e., the Bazel packages
+# that depend on them), adding a step to the pipeline at runtime for each one.
 for pkg in changed_packages:
 
     # Make a step that runs `bazel build` and `bazel test` for this package.
@@ -35,8 +33,7 @@ for pkg in changed_packages:
         # Find the library's reverse dependencies.
         reverse_deps = run(["bazel", "query", f"rdeps(//..., //{pkg}/...)"])
 
-        # Filter this list to exclude any package that already belongs to the
-        # changed_packages set. (There's no need to build these a second time.)
+        # Filter this list to exclude any package that's already set to be built. 
         reverse_deps_to_build = [
             p for p in get_paths(reverse_deps, pkg) if p not in changed_packages
         ]
@@ -44,10 +41,11 @@ for pkg in changed_packages:
         for dep in reverse_deps_to_build:
             rdep_step = get_package_step(dep)
 
-            # Append a step at runtime to the changed_package's list to build
-            # and test the reverse dependency also.
+            # Add a command to the changed library's command list to generate a
+            # new build step (at runtime) for the dependent package and append
+            # it to the pipeline.
             package_step["commands"].extend([
-                f"echo 'Adding a step to the pipeline for {dep}...'",
+                f"echo 'Generating and uploading a follow-up step to build {dep}...'",
                 f"python3 .buildkite/step.py {dep} | buildkite-agent pipeline upload"
             ])
 
